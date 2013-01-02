71c4dfab88af26f9b7223794b74108196f66700c
Ana Arias <acarias@espol.edu.ec>
2013-01-01 19:38:53 -0500

    Agregando funcionalidades módulo "memoriza"

--- EntregaTuTarea/PythonProject/src/memoriza.py
+++ EntregaTuTarea/PythonProject/src/memoriza.py
@@ -45,3 +45,76 @@ class Memoria(state.State):
         print(self.respuesta)
     def exit(self):
         self.instru.stop()
+        
+    def reason(self):
+        if self.teclas == str(self.respuesta):
+            self.correcto= True
+            print(self.random)
+            print(self.nivel)
+            return anios.Anios(self.nivel)
+        
+        elif self.teclas != str(self.respuesta) and self.termino and not self.correcto:
+            print(self.respuesta)
+            print("no entro")
+            self.oportunidad.play()
+            self.delayopor.__init__
+            self.oportunidades +=1
+            
+            if self.oportunidades > 3:
+                print(self.teclas)
+                print(self.respuesta)
+                print(self.random)
+                print(self.nivel)
+                self.perdio.play()
+                self.delayper.__init__
+                return title.Title()
+            else:
+                self.letras=""
+                self.termino=False
+                self.correcto=False
+                self.nombre=""
+
+    def act(self):
+        
+            
+        self.timer.tick(60)
+        surface_manager.clear(self.display, self.background)
+        self.display.blit(self.background, (0, 0))
+        pygame.display.flip()
+        if not self.termino:  
+            eventos=pygame.event.get()
+            for evento in eventos:
+                if evento.type == pygame.KEYDOWN:
+                    if evento.key == pygame.K_F5:
+                        self.teclas = self.nombre
+                        self.termino = True
+                        print(self.teclas)
+                    elif evento.key == pygame.K_BACKSPACE and len(self.nombre)>0:
+                        self.nombre= self.nombre[0:len(self.nombre)-1]
+                    else:
+                        self.nombre= self.nombre + pygame.key.name(evento.key)
+                        print(self.nombre)
+            
+        
+    def seleccion_memoria(self):
+       
+        if self.random2 == 0 and self.nivel == 2:
+                self.memoria1.play()
+                self.respuesta= "7e563"
+        
+        if self.random2 == 1 and self.nivel == 2:
+                self.memoria2.play()
+                self.respuesta= "fw496"
+        
+        if self.random2 == 2 and self.nivel == 2:
+                self.memoria3.play()
+                self.respuesta= "12pkf"
+        
+        if self.random == 0 and self.nivel == 1:
+                self.memoria4.play()
+                self.respuesta= "2463"
+        
+        if self.random == 1 and self.nivel == 1:
+                self.memoria5.play()
+                self.respuesta= "1256"
+    
