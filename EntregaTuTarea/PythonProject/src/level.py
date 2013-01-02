7bf43a622892149ff9dd400ec722f9baf7c51efb
Cesar E. San Lucas A. <slace.93@gmail.com>
2013-01-01 22:41:01 -0500

    Modificación al módulo level.py
    Añadiendo el nivel de escuela

--- EntregaTuTarea/PythonProject/src/level.py
+++ EntregaTuTarea/PythonProject/src/level.py
@@ -66,3 +66,50 @@ class Level(state.State):
             if not surface_manager.has(platform):
                 self.current_platforms.remove(platform)
 
+
+class Level_Escuela(state.State):
+    surface_manager = pygame.sprite.RenderUpdates()
+    def __init__(self):
+        self.display = pygame.display.get_surface()
+        self.background = pygame.image.load("data/images/city.png")
+        self.display.blit(self.background, (0, 0))
+        self.lista = self.lista_p()
+        self.esc_cargado = False
+        self.enter()
+
+    def enter(self):
+        pass
+        #new_platform = platform.EscuelaPlatform("data/images/plataforma_2.png",self.lista[0])
+        #surface_manager.add(new_platform)
+        '''self.current_platforms.append(new_platform)
+        self.time_since_last_powerup = time.clock()
+        self.time_since_last_enemyspawn = time.clock()
+        self.time_since_last_sadspawn = time.clock()'''
+
+    def exit(self):
+        surface_manager.empty()
+
+    def act(self):
+        if not self.esc_cargado:
+            for lista_p in self.lista:
+                new_platform = plataforma.EscuelaPlatform("data/images/plataforma_2.png",lista_p)
+                surface_manager.add(new_platform)
+                enemigos_L2 = enemigo.Enemy_L2(lista_p)
+                surface_manager.add(enemigos_L2)
+            self.esc_cargado = True
+            
+    
+    def lista_p(self):
+        lista = [ [0,self.display.get_height(),self.display.get_width()],
+                  [0,self.display.get_height()*7/8,self.display.get_width()/3],
+                  [self.display.get_width()*2/3,self.display.get_height()*7/8,self.display.get_width()/3],
+                  [self.display.get_width()/12,self.display.get_height()*6/8,self.display.get_width()*2/12],
+                  [self.display.get_width()*7/16,self.display.get_height()*6/8,self.display.get_width()*2/17],
+                  [self.display.get_width()*21/34,self.display.get_height()*33/50,self.display.get_width()*13/100],
+                  [self.display.get_width()*7/10,self.display.get_height()*4/8,self.display.get_width()*3/10],
+                  [self.display.get_width()/2,self.display.get_height()*17/40,self.display.get_width()*13/100],
+                  [self.display.get_width()/12,self.display.get_height()*11/40,self.display.get_width()*9/25]]
+                 
+        return lista
+    
+
