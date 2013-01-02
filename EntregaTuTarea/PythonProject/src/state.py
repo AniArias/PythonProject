05bbf6e6cd28a1535c477309ae6677b0df76350b
Cesar E. San Lucas A. <slace.93@gmail.com>
2013-01-01 21:26:18 -0500

    Modificando modulo state.py
    Nueva clase se usará para la actualización de algún estado actual o de
    ser el caso en la transición a un nuevo estado del juego

--- EntregaTuTarea/PythonProject/src/state.py
+++ EntregaTuTarea/PythonProject/src/state.py
@@ -14,3 +14,24 @@ class State(object):
         pass
 
 
+class StateMachine(object):
+
+    def __init__(self, host, first_state=None):
+        self.host = host
+        self.current_state = first_state
+
+    def transition(self, new_state):
+        self.current_state.exit()
+        self.current_state = new_state
+        self.current_state.host = self.host
+        self.current_state.fsm = self
+        self.current_state.enter()
+
+    def update(self):
+        if self.current_state:
+            new_state = self.current_state.reason()
+
+            if new_state: 
+                self.transition(new_state)
+            else:
+                self.current_state.act()
