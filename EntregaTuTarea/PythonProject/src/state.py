b47b5e274b591a3b09545ef45f16478ddc642375
Cesar E. San Lucas A. <slace.93@gmail.com>
2013-01-01 21:21:34 -0500

    Creando módulo state
    Contiene la clase state se usará para herencia en unas clases usadas por
    nuestra app para evitar así en el cambio de estados variables no
    definidas o cualquier error por causa de  inicialización

---
+++ EntregaTuTarea/PythonProject/src/state.py
@@ -0,0 +1,16 @@
+class State(object):
+    def __init__(self):
+        pass
+    def enter(self):
+        pass
+
+    def exit(self):
+        pass
+
+    def reason(self):
+        pass
+
+    def act(self):
+        pass
+
+
