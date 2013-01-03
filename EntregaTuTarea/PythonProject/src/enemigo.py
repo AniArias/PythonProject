bafa2b45402d92f8de4ed9188674d5d2f8cfac2a
Ana Arias <acarias@espol.edu.ec>
2013-01-02 22:46:19 -0500

    Agregando clase interna de módulo "enemigo"

--- EntregaTuTarea/PythonProject/src/enemigo.py
+++ EntregaTuTarea/PythonProject/src/enemigo.py
@@ -50,3 +50,73 @@ class Enemigo(pygame.sprite.DirtySprite):
                 self.hit_sound.play()
                 game.update_caritas()
 
+
+class Enemy_L2(pygame.sprite.DirtySprite):
+    def __init__(self,lista):
+        super(Enemy_L2, self).__init__()
+        self.display = pygame.display.get_surface()
+        enemy_sprite = pygame.image.load("data/images/enemy_frame4.png").convert_alpha()
+        self.image = pygame.transform.flip(enemy_sprite, True, False)
+        self.is_hit = False
+        self.posx_plat = lista[0]
+        self.pos_x = lista[0] 
+        self.pos_y = lista[1] - ( 10+self.image.get_height())
+        self.anc_plat = self.pos_x + lista[2]
+        self.rect = pygame.Rect(self.pos_x, self.pos_y, self.image.get_width(), self.image.get_height())
+        self.is_falling = True
+        self.is_mov_der = True
+        self.hit_sound = pygame.mixer.Sound("data/sound/hit.wav")
+        self.dirty = 1
+
+    def update(self):
+        self.check_if_hit()
+        
+        if self.is_hit:
+            surface_manager.remove(self)
+        
+        if self.on_platform_L2():
+            self.is_falling = False
+            
+            if self.pos_x < self.anc_plat and self.is_mov_der:
+                self.pos_x += 3
+                
+            if self.pos_x >= self.anc_plat:
+                self.is_mov_der = False
+            
+            if self.pos_x > self.posx_plat and not self.is_mov_der:
+                self.pos_x -= 3
+            
+            if self.pos_x <= self.posx_plat:
+                self.is_mov_der = True
+
+        if self.is_falling:
+            self.pos_y += 8
+            self.dirty = 1
+            
+        self.rect.topleft = (self.pos_x, self.pos_y)
+        
+    def check_if_hit(self):
+        if self.is_hit:
+            return
+        collidelist = pygame.sprite.spritecollide(self, surface_manager.surface_list, False)
+
+        for item in collidelist:
+            if type(item) is arma.Arma:
+                surface_manager.remove(item)
+                self.is_hit = True
+                self.image = pygame.transform.flip(self.image, False, True)
+                self.hit_sound.play()
+    
+    def on_platform_L2(self):
+        collidelist = pygame.sprite.spritecollide(self, surface_manager.surface_list, False)
+    
+        for item in collidelist:
+            if type(item) is player.Player:
+                item.current_life -=1
+                continue
+            if type(item) is plataforma.EscuelaPlatform:
+                if (self.pos_y + self.rect.height) <= (item.pos_y + 8) and (self.pos_x + self.rect.width) >= item.pos_x :
+                    return True
+                
+        return False
+
