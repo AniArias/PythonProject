35a32ad2533c149ffd9c0052dd7c8ee014ddae4e
Ana Arias <acarias@espol.edu.ec>
2013-01-02 22:45:23 -0500

    Agregando funcionalidades a módulo "enemigo"

--- EntregaTuTarea/PythonProject/src/enemigo.py
+++ EntregaTuTarea/PythonProject/src/enemigo.py
@@ -19,3 +19,34 @@ class Enemigo(pygame.sprite.DirtySprite):
         self.is_hit = False
         self.hit_sound = pygame.mixer.Sound("data/sound/hit.wav")
         self.dirty = 1
+        
+    def update(self):
+        if self.pos_x < 0 - self.rect.width:
+            surface_manager.remove(self)
+
+        self.check_if_hit()
+
+        if self.is_hit:
+            self.pos_y += 10
+            if self.pos_y >= self.display.get_height():
+                surface_manager.remove(self)
+        if not self.is_hit:                
+            self.pos_x += self.velx
+            self.pos_y += self.vely
+
+        self.rect.topleft = (self.pos_x, self.pos_y)
+        self.dirty = 1
+
+    def check_if_hit(self):
+        if self.is_hit:
+            return
+        collidelist = pygame.sprite.spritecollide(self, surface_manager.surface_list, False)
+
+        for item in collidelist:
+            if type(item) is arma.Arma:
+                surface_manager.remove(item)
+                self.is_hit = True
+                self.image = pygame.transform.flip(self.image, False, True)
+                self.hit_sound.play()
+                game.update_caritas()
+
